33c5fb069d9f152f4fe040551dcb7033fdc1a325
zdzicho <ania@op.pl>
2023-09-14 17:13:06 +0200

    Add object of domain - Order Class

---
+++ GitHub_Ksiazka/MyProject04/src/main/java/my/app/com/MyProject04/domain/Order.java
@@ -0,0 +1,53 @@
+package my.app.com.MyProject04.domain;
+
+import javax.persistence.*;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.CreditCardNumber;
+
+@Entity
+@Data
+@Table(name = "Order_Taco")
+public class Order implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private Date placeAt;
+
+    @NotBlank(message = "delivery name cannot be blank")
+    private String deliveryName;
+    @NotBlank(message = "delivery street cannot be blank")
+    private String deliveryStreet;
+    @NotBlank(message = "delivery city cannot be blank")
+    private String deliveryCity;
+    @NotBlank(message = "delivery state cannot be blank")
+    private String deliveryState;
+    @NotBlank(message = "delivery zip cannot be blank")
+    private String deliveryZip;
+
+    @CreditCardNumber(message = "Please insert correct Credit Card Number")
+    private String ccNumber;
+    @Pattern(regexp="\\D")
+    private String ccExpiration;
+    @Digits(integer = 3,fraction = 0,message = "Please insert correct cvv number")
+    private String ccCVV;
+
+    @ManyToMany(targetEntity = Taco.class)
+    private List<Taco> tacos = new ArrayList<>();
+
+    @PrePersist
+    public void setPlaceAtDate(){
+        this.placeAt = new Date();
+    }
+}
