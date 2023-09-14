0f98f70d073281dd6e80fdd7caf2d23ec820a602
zdzicho <ania@op.pl>
2023-09-14 16:39:39 +0200

    Add object of domain Taco Class

---
+++ GitHub_Ksiazka/MyProject04/src/main/java/my/app/com/MyProject04/domain/Taco.java
@@ -0,0 +1,31 @@
+package my.app.com.MyProject04.domain;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+import javax.validation.constraints.*;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Taco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull(message = "name cannot be blank")
+    @Size(min = 5,message = "Name must have at least 5 characters")
+    private String name;
+
+    private Date createAt;
+
+    @Size(min = 1,message = "Taco must have at least one ingredient")
+    @ManyToMany(targetEntity = Ingredient.class)
+    private List<Ingredient> ingredients;
+
+    @PrePersist
+    private void createAt(){
+        this.createAt = new Date();
+    }
+}
+
